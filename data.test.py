e95ac3b462300985b06e4a78893edca0c65a5cb9
Rasmus Lehmann <rale@itu.dk>
2019-11-01 14:06:50 +0100

    First shipable version.2

--- data.test.py
+++ data.test.py
@@ -3,5 +3,5 @@ import json
 with open('data/10-09-19.txt') as in_file:
     data = json.loads(in_file.readline().split('\t')[1])
     for p in data:
-        print data[p]['full_text']
+        print(data[p]['full_text'])
 
